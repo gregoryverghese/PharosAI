f6a64527ff163c851bcdcf2869c90fda32b3b282
gregoryverghese <gregory.verghese@gmail.com>
2023-08-11 14:40:24 +0100

    updated routes so time_since#index is accessed when users visit teh route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'time_since#index'
+  
   get 'time_since', to: 'time_since#index'
 
   # Defines the root path route ("/")
