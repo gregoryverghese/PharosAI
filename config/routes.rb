3ac7522decc3923ec0017ec2fc8af1da770e6349
gregoryverghese <greg.verghese@gmail.com>
2024-05-23 19:45:19 +0100

    fixed the routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
 
   root to: redirect('/index.html')
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'time_since#index'
-  
-  get 'time_since', to: 'time_since#index'
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
