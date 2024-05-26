be2d91a56bf84c4f174b31827a27515ac21ad431
gregoryverghese <greg.verghese@gmail.com>
2024-05-26 13:16:09 +0100

    cursor disappears at the ned

--- public/script.js
+++ public/script.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const text = "Navigating the path to a cancer free world.";
+  const text = "Navigating the path to a cancer free world";
   let index = 0;
   const speed = 100; // typing speed in milliseconds
+  const typingText = document.getElementById("typing-text");
 
   function type() {
     if (index < text.length) {
-      document.getElementById("typing-text").textContent += text.charAt(index);
+      typingText.textContent += text.charAt(index);
       index++;
       setTimeout(type, speed);
+    } else {
+      // Remove the cursor after typing is complete
+      typingText.parentElement.classList.add('blink-off');
     }
   }
 
